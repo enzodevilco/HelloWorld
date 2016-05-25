ee12d6b78b348bcb9e9707a48584a59991bba817
Melcom Engbwang <Trackplayer@MacBook-Pro-de-Melcom.local>
2016-05-25 20:09:02 +0200

    modif

--- Hello2/ViewController.swift
+++ Hello2/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated. bonjour 
+        
     }
 
 
