448195a37916ca83946e0497b31248fc12b6617f
Junior Wayou <Junior@MacBook-Pro-de-Junior-2.local>
2016-05-25 20:46:30 +0200

    ouloulou

--- Hello2/ViewController.swift
+++ Hello2/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated. bonjour " boui boui
+        // Dispose of any resources that can be recreated. bonjour " boui boui ouloulou
 
         
     }
